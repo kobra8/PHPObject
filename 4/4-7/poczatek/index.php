b7cdabcbf17e88050fc6c22cb185bfa2d3fa4108
Jerzy Drobot <jerzydrobot@gmail.com>
2017-12-03 17:22:42 +0100

    Interfaces and abstract class

--- 4/4-7/poczatek/index.php
+++ 4/4-7/poczatek/index.php
@@ -1,5 +1,45 @@
 <?php
 
+//Przygotowanie interfejsu
 
+interface Database {
+  public function openConnection();
+  public function prepareQuery();
+}
+
+//Implementacja interfejsu dla klasy MySQL
+
+class MySQL implements Database {
+  public function openConnection() {
+    echo 'Przygotowuję zapytanie do bazy MySQL<br>';
+  }
+  public function prepareQuery() {
+    echo 'Otwieram połączenie z bazą MySQL<br>';
+  }
+}
+//Implementacja interfejsu dla klasy SQLite
+
+class SQLite implements Database {
+  public function openConnection() {
+    echo 'Przygotowuję zapytanie do bazy SQLite<br>';
+  }
+  public function prepareQuery() {
+    echo 'Otwieram połączenie z bazą SQLite<br>';
+  }
+}
+
+/* Tworząc nowy obiekt danej klasy używamy nazw metod zdefiniowanych w interfejsie
+  nie martwiąc się tutaj o szczegóły ich implementacji
+*/
+
+//Nowy obiekt klasy SQLite 
+$db1 = new SQLite();
+$db1->openConnection(); // Nazwa metody z interfejsu
+$db1->prepareQuery(); // Nazwa metody z interfejsu
+
+//Nowy obiekt klasy MySQL
+$db2 = new MySQL();
+$db2->openConnection(); // Nazwa metody z interfejsu
+$db2->prepareQuery(); // Nazwa metody z interfejsu
 
 ?>
--- 4/4-9/poczatek/index.php
+++ 4/4-9/poczatek/index.php
@@ -1,5 +1,38 @@
 <?php
 
+abstract class Shape {
+  public abstract function getArea();
+}
 
+class Rectangle extends Shape {
+  private $a;
+  private $b;
+
+  public function __construct($a, $b) {
+    $this->a =$a;
+    $this->b =$b;
+  }
+
+  public function getArea() {
+    return $this->a * $this->b;
+  }
+}
+
+class Circle extends Shape {
+  private $radius;
+
+  public function __construct($radius) {
+    $this->radius = $radius;
+  }
+  public function getArea() {
+    return 3.14 * pow($this->radius, 2);
+  }
+}
+
+$rectangle = new Rectangle(2, 4);
+echo $rectangle->getArea();
+echo'<br>';
+$circle = new Circle(10);
+echo $circle->getArea();
 
 ?>
